c156bb168bd1cce05672715709f3a1a289107e6c
diegodileo3 <diegodileo3@gmail.com>
2020-05-27 20:55:45 -0300

    Creo clase en archivo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,1 +1,7 @@
-System.out.println("Hola mundo");
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+	System.out.println("How are you?");
+	System.out.println("Fine Thanks");
+    }
+}
